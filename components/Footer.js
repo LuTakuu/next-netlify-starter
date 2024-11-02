981730063c9267136bbb54db56d4ca89da29d7c5
Lucas Oberstaller <lucas.oberstaller@it.htl-hl.ac.at>
2024-11-02 13:57:41 +0100

    wip

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <img src="/construction.jpg" alt="construction worker" className={styles.logo} />
       </footer>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="🚧 Work in Progress 🚧" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+            This site is currently under construction. Please check back soon!
         </p>
       </main>
 
